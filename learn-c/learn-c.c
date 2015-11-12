afa50b39e37c222e23667eea33d1810e8d7c44de
Sherif Eldeeb <archeldeeb@gmail.com>
2015-11-12 17:51:09 +0300

    02 - puts

--- learn-c/learn-c.c
+++ learn-c/learn-c.c
@@ -1,7 +1,11 @@
 // learn-c.c : Defines the entry point for the console application.
 //
 
+#include <stdio.h>
+
 int main()
 {
+	char greetings[] = "Hi there!";
+	puts(greetings);
     return 0;
 }
