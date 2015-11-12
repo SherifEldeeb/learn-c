9a9f873c02f59a3ea94b0ba809548b3a815cbf85
Sherif Eldeeb <archeldeeb@gmail.com>
2015-11-12 18:19:42 +0300

    04 - pointers

--- learn-c/learn-c.c
+++ learn-c/learn-c.c
@@ -8,13 +8,23 @@ int main()
 	int c;
 	char c2;
 
+	int* c_ptr;		// pointer to an int
+	char* c2_ptr;	// pointer to a char
+
 	char greetings[] = "Hi there!, please type a single character";
 	puts(greetings);
 
 	c = getchar();
 	printf("You typed '%c', which is '%x' in hex, and '%d' in dec.\n", c, c, c);
 
-	c2 = (char)c;
+	c2 = (char)c; // casting the value of c as a char
 	printf("After casting 'c' value as a char, c2 value is %c\n", c2);
-    return 0;
+
+	c_ptr = &c; 
+	c2_ptr = &c2;
+
+	printf("The variable 'c' is at address '0x%08x'\n", c_ptr);
+  	printf("The variable 'c2' is at address '0x%08x'\n", c2_ptr);
+  	
+	return 0;
 }
