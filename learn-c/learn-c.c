3a44de3860f81df8fc8f3a17b8ce8483f6b98cff
Sherif Eldeeb <archeldeeb@gmail.com>
2015-11-12 18:53:37 +0300

    06 - if else

--- learn-c/learn-c.c
+++ learn-c/learn-c.c
@@ -12,7 +12,20 @@ int main()
 	printf("Url: %s will be saved to '%s'\n", url, path);
 
 	// https://msdn.microsoft.com/en-us/library/ms775123(v=vs.85).aspx
-	URLDownloadToFileA(NULL, url, path, 0, NULL);
+	HRESULT result = URLDownloadToFileA(NULL, url, path, 0, NULL);
+
+	if (result == S_OK) {
+		printf("Ok\n");
+	}
+	else if (result == E_OUTOFMEMORY) {
+		printf("Buffer length invalid, or insufficient memory\n");
+	}
+	else if (result == INET_E_DOWNLOAD_FAILURE) {
+		printf("URL is invalid\n");
+	}
+	else {
+		printf("Other error: %d\n", result);
+	}
 
 	return 0;
 }
