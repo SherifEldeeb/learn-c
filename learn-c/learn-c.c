25b543a00f8a8262c9e7f94ef18c00c0d476b7f9
Sherif Eldeeb <archeldeeb@gmail.com>
2015-11-12 18:03:42 +0300

    03 - casting

--- learn-c/learn-c.c
+++ learn-c/learn-c.c
@@ -5,7 +5,16 @@
 
 int main()
 {
-	char greetings[] = "Hi there!";
+	int c;
+	char c2;
+
+	char greetings[] = "Hi there!, please type a single character";
 	puts(greetings);
+
+	c = getchar();
+	printf("You typed '%c', which is '%x' in hex, and '%d' in dec.\n", c, c, c);
+
+	c2 = (char)c;
+	printf("After casting 'c' value as a char, c2 value is %c\n", c2);
     return 0;
 }
