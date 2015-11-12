61e5c03a88b618948e457899b6cbe6825e29fee2
Sherif Eldeeb <archeldeeb@gmail.com>
2015-11-12 18:49:09 +0300

    05 - URLDownloadToFileA

--- learn-c/learn-c.c
+++ learn-c/learn-c.c
@@ -2,29 +2,17 @@
 //
 
 #include <stdio.h>
+#include <Urlmon.h>
 
 int main()
 {
-	int c;
-	char c2;
+	char url[] = "http://0up.me/file.txt";
+	char path[] = "c:\\temp\\file.txt";
 
-	int* c_ptr;		// pointer to an int
-	char* c2_ptr;	// pointer to a char
+	printf("Url: %s will be saved to '%s'\n", url, path);
 
-	char greetings[] = "Hi there!, please type a single character";
-	puts(greetings);
+	// https://msdn.microsoft.com/en-us/library/ms775123(v=vs.85).aspx
+	URLDownloadToFileA(NULL, url, path, 0, NULL);
 
-	c = getchar();
-	printf("You typed '%c', which is '%x' in hex, and '%d' in dec.\n", c, c, c);
-
-	c2 = (char)c; // casting the value of c as a char
-	printf("After casting 'c' value as a char, c2 value is %c\n", c2);
-
-	c_ptr = &c; 
-	c2_ptr = &c2;
-
-	printf("The variable 'c' is at address '0x%08x'\n", c_ptr);
-  	printf("The variable 'c2' is at address '0x%08x'\n", c2_ptr);
-  	
 	return 0;
 }
